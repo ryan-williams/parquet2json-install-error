794e8a6a3f1ba2f4db7e81a60a1d72a3073f1904
Ryan Williams <ryan@runsascoded.com>
2023-10-23 10:50:02 -0400

    rm git subdir files

--- rc/git/test/github_test.py
+++
@@ -1,8 +0,0 @@
-from util import github_util
-
-from nose.tools import *
-
-def test_make_github_url_github_pages():
-    remote = 'danvk/danvk.github.io.git'
-    eq_('https://github.com/danvk/danvk.github.io',
-        github_util.make_github_url(remote))
--- rc/git/util/root.py
+++
@@ -1,6 +0,0 @@
-
-import subprocess
-
-def git_root():
-    return subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip()
-
